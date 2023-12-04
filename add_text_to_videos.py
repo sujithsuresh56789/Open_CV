52b23771a5e5942a3e363b28358c2fa78e170be5
Sujith <sujithsuresh56789@gmail.com>
2023-12-04 14:13:28 +0530

    first commit

---
+++ add_text_to_videos.py
@@ -0,0 +1,22 @@
+import cv2
+cap=cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,3200)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,3000 )
+
+print(cap.get(3))
+print(cap.get(4))
+
+while(True):
+    ret,frame=cap.read()
+    if ret == True:
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow("frame",gray)
+        if cv2.waitKey(1)==ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
